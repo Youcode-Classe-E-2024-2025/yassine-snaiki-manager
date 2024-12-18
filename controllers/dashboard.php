c9e50fb57a4bb56d45f33cf7400c497e3ee66f31
yassine snaiki <yassnaiki@gmail.com>
2024-12-18 16:52:42 +0100

    adding statistics and movements to user page

--- controllers/dashboard.php
+++ controllers/dashboard.php
@@ -7,5 +7,19 @@ if($_SESSION['role']==='a')
 header('Location: /admin-dashboard');
 
 $profileName = $_SESSION['name'];
+global $db;
+
+// $user_info = $db->query("SELECT balance , totaldeposits, totalwithdrawals FROM userinfo where user_id = ?",[$_SESSION['user_id']])->fetch();
+
+$movements = $db->query("SELECT amount, TO_CHAR(created_at, 'YYYY-MM-DD HH24:MI:SS') created_at , type FROM movements where user_id = ?;", [$_SESSION['user_id']])->fetchAll(PDO::FETCH_ASSOC);
+$balance=0;
+$totaldep = 0;
+$totalwith = 0;
+foreach($movements as $movement) {
+    if($movement['type'] === 'd') $totaldep += $movement['amount'];
+    else $totalwith += $movement['amount'];
+}
+$balance = $totaldep - $totalwith;
+
 
 require "views/dashboard.view.php";
--- views/dashboard.view.php
+++ views/dashboard.view.php
@@ -2,27 +2,44 @@
 <?php require "views/partials/head.php" ?>
 <div class="flex h-[100dvh]">
 <?php require "views/partials/sidebar.php" ?>
-<main class=" basis-full p-10">
+<main class=" basis-full p-10 overflow-auto">
     <h2 class="text-4xl font-bold">Dashboard</h2>
     <ul class="flex justify-between mt-10 text-lg font-semibold">
         <div class="basis-[30%]">
             <h3 class="text-center mb-4">Balance</h3>
             <div class="w-full bg-cyan-600 h-20 rounded-sm flex justify-center items-center text-4xl font-semibold">
-                $450.00
+                $<?=$balance?>
             </div>
         </div>
         <div class="basis-[30%]">
             <h3 class="text-center mb-4">Total deposits</h3>
             <div class="w-full bg-cyan-600 h-20 rounded-sm flex justify-center items-center text-4xl font-semibold">
-                $450.00
+            $<?=$totaldep?>
             </div>
         </div>
         <div class="basis-[30%]">
             <h3 class="text-center mb-4">total withdrawals</h3>
             <div class="w-full bg-cyan-600 h-20 rounded-sm flex justify-center items-center text-4xl font-semibold">
-                $450.00
+            $<?=$totalwith?>
             </div>
         </div>
+        
+    </ul>
+    <h2 class="text-2xl font-semibold text-center mt-20">Movements</h2>
+    <ul class="flex flex-col w-full gap-1 justify-between mt-10 text-lg font-semibold ">
+    <?php 
+    if(count($movements) === 0)
+           echo "<li class='flex items-center text-xl font-semibold bg-cyan-600 px-10 justify-between'>
+                 No movements yet
+                 </li>";
+    foreach($movements as $movement) : ?>
+            <li class="flex items-center text-xl font-semibold bg-cyan-600 px-10 justify-between">
+                <p>$<?=$movement['amount']?></p>
+                <p><?=$movement['created_at']?></p>
+                <p><?=$movement['type']?></p>
+                
+            </li>
+    <?php endforeach?>
         
     </ul>
 </main>
