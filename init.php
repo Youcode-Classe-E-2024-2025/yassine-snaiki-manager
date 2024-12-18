b823b7fd449bbd25895d792a7dc2390c2bd83c84
yassine snaiki <yassnaiki@gmail.com>
2024-12-18 11:12:34 +0100

    adding queried fields

--- init.php
+++ init.php
@@ -7,7 +7,7 @@ $rowsCount = $db->query('SELECT COUNT(*) FROM users')->fetch()['count'];
 if($rowsCount === 0) {
     $hashedPassword = password_hash('admin', PASSWORD_DEFAULT);
     try {
-        $id = $db->query("INSERT INTO users(name,email,password) VALUES (?,?,?) RETURNING id;",['admin','admin@gmail.com',$hashedPassword])->fetch()['id'];
+        $id = $db->query("INSERT INTO users(name,email,password,isconfirmed) VALUES (?,?,?) RETURNING id;",['admin','admin@gmail.com',$hashedPassword,true])->fetch()['id'];
         $db->query("INSERT INTO roles(name,user_id) VALUES(?,?)",['a',$id]);
     }catch(PDOException $err) {
         echo "$err failed to execute query";
